0b7dafa2e946cdd4ccd2ee6dbc39866fa7fd8f80
912i <pokemanialasuuper@gmail.com>
2026-04-06 02:46:40 +0200

    Finalisation du Modèle : Personnage, Joueur et logique de stats/classes

--- Aelia/Modele/Personnage.cs
+++ Aelia/Modele/Personnage.cs
@@ -1,45 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Security.Cryptography;
+﻿namespace Aelia.Modele;
 
 public class Personnage
 {
+    public static readonly string[] RacesDisponibles = { "Humain", "Elfe", "Nain", "Drow" };
+    public static readonly string[] ClassesDisponibles = { "Guerrier", "Mage", "Voleur", "Paladin"};
+
+    // Propriétés
     public string Nom { get; set; }
     public int Age { get; set; }
     public string Genre { get; set; }
     public string Metier { get; set; }
     public string Race { get; set; }
-    public List<string> Inventaire { get; set; }
-    public Statistique Statistiques { get; set; }
+    public List<string> Classe { get; set; } = new List<string>();
+    public List<string> Inventaire { get; set; } = new List<string>();
+    public Statistique Statistiques { get; set; } = new Statistique();
+    
+    protected static readonly Random rnd = new Random();
 
     public bool Roll(int maStat)
     {
-        Random rnd = new Random();
-        List<int> Des = new List<int>(2);
-        Des.Add(rnd.Next(1, 20));
-        Des.Add(rnd.Next(1, 20));
+        int de1 = rnd.Next(1, 21); 
+        int de2 = rnd.Next(1, 21);
 
-        int avantage = Math.Max(Des[0], Des[1]);
+        int avantage = Math.Max(de1, de2);
         return maStat >= avantage;
     }
-    
 }
 
 public class Joueur : Personnage
 {
+    public void DefinitionStatistique()
+    {
+        // Utilisation du hasard pour le pool de départ
+        this.Statistiques.Force = rnd.Next(1, 11) + 2;
+        this.Statistiques.Intelligence = rnd.Next(1, 11) + 2;
+        this.Statistiques.Resistance = rnd.Next(1, 11) + 2;
+        this.Statistiques.Perception = rnd.Next(1, 11) + 2;
+        this.Statistiques.Vitesse = rnd.Next(1, 11) + 2;
+    }
     
+    public void AjouterInventaire(string objet) => Inventaire.Add(objet);
+
+    public void DefinirNom(string nom) => this.Nom = nom;
+
+    public void DefinirAge(int age) => this.Age = age;
+
+    // Utilisation de l'index pour piocher dans le tableau statique
+    public void DefinirRace(int indexRace)
+    {
+        if (indexRace >= 0 && indexRace < RacesDisponibles.Length)
+        {
+            this.Race = RacesDisponibles[indexRace];
+        }
+    }
+
+    public void DefinirClasse(int indexClasse)
+    {
+        if (indexClasse >= 0 && indexClasse < ClassesDisponibles.Length)
+        {
+            string nouvelleClasse = ClassesDisponibles[indexClasse];
+            if (!this.Classe.Contains(nouvelleClasse))
+            {
+                this.Classe.Add(nouvelleClasse);
+            }
+        }
+    }
 }
 
 public class PNJ : Personnage
 {
-    
-}
 
-public class Statistique
-{
-    public int Force { get; set; }
-    public int Perception { get; set; }
-    public int Intelligence { get; set; }
-    public int Vitesse { get; set; }
-    public int Resistance { get; set; }
 }
---
+++ Aelia/Modele/Statistique.cs
@@ -0,0 +1,41 @@
+﻿namespace Aelia.Modele;
+
+public class Statistique
+{
+
+    private int _force;
+    private int _perception;
+    private int _intelligence;
+    private int _vitesse;
+    private int _resistance;
+
+    public int Force
+    {
+        get => _force;
+        set => _force = Math.Clamp(value, 0, 20);
+    }
+
+    public int Perception
+    {
+        get => _perception;
+        set => _perception = Math.Clamp(value, 0, 20);
+    }
+
+    public int Intelligence
+    {
+        get => _intelligence;
+        set => _intelligence = Math.Clamp(value, 0, 20);
+    }
+
+    public int Vitesse
+    {
+        get => _vitesse;
+        set => _vitesse = Math.Clamp(value, 0, 20);
+    }
+
+    public int Resistance
+    {
+        get => _resistance;
+        set => _resistance = Math.Clamp(value, 0, 20);
+    }
+}

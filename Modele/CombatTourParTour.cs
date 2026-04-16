d3b70533929b75ee6fed448c124da3694ff8dd20
912i <pokemanialasuuper@gmail.com>
2026-04-16 03:21:44 +0200

    feat: ajout de la classe CombatTourParTour pour gérer les combats au tour par tour

---
+++ Modele/CombatTourParTour.cs
@@ -0,0 +1,22 @@
+// Classe représentant un combat au tour par tour
+using Aelia.Modele;
+
+public class CombatTourParTour
+{
+    // Attributs
+    private Personnage personnageJoueur;
+    private Personnage personnageAdversaire;
+
+    // Constructeur
+    public CombatTourParTour(Personnage personnageJoueur, Personnage personnageAdversaire)
+    {
+        this.personnageJoueur = personnageJoueur;
+        this.personnageAdversaire = personnageAdversaire;
+    }
+
+    // Méthode pour lancer un tour de combat
+    public void LancerTourDeCombat()
+    {
+        // À implémenter
+    }
+}
